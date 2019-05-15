ac0340d90ce6b911c831a719658038bc9bb36151
Micro <microa4@gmail.com>
2019-05-15 21:34:15 +0200

    salren el numero de emojis que pongas en el input min 37:11

--- src/app/smiley/smiley.component.ts
+++ src/app/smiley/smiley.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./smiley.component.scss']
 })
 export class SmileyComponent {
+  showNumEmojis: number =Infinity;
   //cramos el array
   emojis = ['😀', '😁', '😂', 
   '😃',  '😄',  '😅', '😆', '😉', 
